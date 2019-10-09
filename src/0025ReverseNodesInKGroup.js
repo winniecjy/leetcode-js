baeb22bfc8d3e80921513f5de4f85459d92eddf0
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-10-10 00:00:14 +0800

    add: 0025

--- src/0025ReverseNodesInKGroup.js
+++ src/0025ReverseNodesInKGroup.js
@@ -6,6 +6,42 @@
  * }
  */
 /**
+ * 第一思路
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+  if (head === null || k<=1) return head;
+  // 1. 检查长度是否大于等于k
+  let nextGroupHead = head;
+  for (let i=1; i<k; i++) {
+    nextGroupHead = nextGroupHead.next;
+    if (nextGroupHead === null) return head;
+  }
+  let reverseNum = 1;
+  let a = head,
+      b = a!==null?a.next:null,
+      c= (a!==null&&b!==null)?b.next:null;
+  a.next = null;
+  while(reverseNum < k && a!==null && b!==null) {
+    reverseNum++;
+    b.next = a;
+    if (reverseNum !== k) {
+       a = b;
+      b = c;
+    console.log(reverseNum, k)
+    if (c!==null) {
+      c = c.next;
+    }
+    }
+  }
+  head.next = reverseKGroup(c, k);
+  return b;
+};
+
+/**
+ * 优化思路
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
@@ -42,12 +78,26 @@ var reverseKGroup = function(head, k) {
 /**
  * 高票思路
  * @param {ListNode} head
+ * @param {number} k
  * @return {ListNode}
  */
-var swapPairs = function(head) {
-  if ((head == null)||(head.next == null)) return head;
-  let n = head.next;
-  head.next = swapPairs(head.next.next);
-  n.next = head;
-  return n;
-};
+var reverseKGroup = function(head, k) {
+  // 得到链表长度
+  let len = 0;
+  for (let i = head; i !== null; len++, i = i.next);
+  let dmy = new ListNode(0);
+  dmy.next = head;
+
+  // 每一组翻转
+  for(let prev = dmy, tail = head; len >= k; len -= k) {
+    for (let i = 1; i < k; i++) {
+      let next = tail.next.next;
+      tail.next.next = prev.next;
+      prev.next = tail.next;
+      tail.next = next;
+    }
+    prev = tail;
+    tail = tail.next;
+  }
+  return dmy.next;
+}
