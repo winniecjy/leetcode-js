bfbf8d1e098015dd607498af248a9752e905522a
caijieying <caijieying@jd.com>
2019-10-15 22:11:05 +0800

    add: 0028

--- analysis/0028ImplementStrStr.md
+++ analysis/0028ImplementStrStr.md
@@ -13,11 +13,18 @@
   说明:
   当目标字符串为空时需要返回什么呢？这在面试的时候是一个好问题，与C中的strstr()和Java中的indexOf()保持一致，此时将会返回0。   
 - **思路**   
-
+一个普通的思路，遍历比较一下，时间复杂度O(n*l)，n为原始字符串长度，l为目标字符串长度。   
+Runtime: 60 ms, faster than 46.63% of JavaScript online submissions for Implement strStr().   
+Memory Usage: 34.9 MB, less than 64.29% of JavaScript online submissions for Implement strStr().   
 
 - **优化思路**   
-无
+实际上第二次循环是不必要的，可以截取出needle长度的子字符串，比较needle和子字符串即可，时间复杂度O(n)。    
+Runtime: 52 ms, faster than 84.97% of JavaScript online submissions for Implement strStr().    
+Memory Usage: 33.8 MB, less than 89.29% of JavaScript online submissions for Implement strStr().    
 
 - **高票答案对比**   
+思路和第一思路是一致的，就是双重循环，由于减少了一些判断，所以数据也好一些。    
+Runtime: 56 ms, faster than 66.80% of JavaScript online submissions for Implement strStr().   
+Memory Usage: 35.2 MB, less than 39.29% of JavaScript online submissions for Implement strStr().   
 
-顺便学习了一个英语成语：needle in a haystack，在干草堆里找针，表示大海捞针，非常困难
+顺便学习了一个英语成语：needle in a haystack，在干草堆里找针，表示大海捞针，非常困难。   
--- src/0028ImplementStrStr.js
+++ src/0028ImplementStrStr.js
@@ -1,8 +1,59 @@
 /**
+ * 第一思路
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    
+var strStrNormal = function(haystack, needle) {
+  if (needle.length == 0) return 0;
+  for(let st=0; st<haystack.length; st++) {
+    if (haystack.length-st < needle.length) break;
+    let success = false;
+    for(let i=0, j=st; i<needle.length && j<haystack.length; i++, j++) {
+      if (needle[i] !== haystack[j]) {
+        break;
+      } else if (i === needle.length-1) {
+        success = true;
+      }
+    }
+    if (success) {
+      return st;
+    }
+  }
+  return -1;
+};
+
+/**
+ * 优化思路
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStrBetter = function(haystack, needle) {
+  if (needle.length == 0) return 0;
+  let needleLen = needle.length;
+  let haystackLen = haystack.length;
+  for(let st=0; st<haystackLen; st++) {
+    if (haystackLen-st < needleLen) break;
+    if(haystack.substr(st, needleLen) === needle) {
+      return st;
+    }
+  }
+  return -1;
+};
+
+/**
+ * 高票思路
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStrVoted = function(haystack, needle) {
+  for (let i = 0; ; i++) {
+    for (let j = 0; ; j++) {
+      if (j == needle.length) return i;
+      if (i + j == haystack.length) return -1;
+      if (needle.charAt(j) != haystack.charAt(i + j)) break;
+    }
+  }
 };
