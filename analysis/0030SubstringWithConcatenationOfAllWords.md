13e41e6c1d465d55f1eef140b0230d8302402e08
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-10-27 16:58:50 +0800

    add: 0030

--- analysis/0030SubstringWithConcatenationOfAllWords.md
+++ analysis/0030SubstringWithConcatenationOfAllWords.md
@@ -1,8 +1,6 @@
 ## 题解：[20191023]#0030 Substring with Concatenation of All Words
 - **题干**   
 给出一个字符串s和一个单词数组，每个单词的长度是一致的，找出s中恰好可以由words中所有单词串联形成的子串的起始位置。注意子串要与 words 中的单词完全匹配，中间不能有其他字符，不需要考虑words中单词串联的顺序。    
-You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
-给出一个除数和被除数，返回被除数除以除数的商，要求不使用乘法、除法和模运算。   
     示例：   
     ```javascript
     // e.g.1
@@ -19,10 +17,17 @@ You are given a string, s, and a list of words, words, that are all of the same 
     ``` 
     
 - **思路**   
-
+第一思路首先尝试了一种大概率会超时的方案，循环words数组，找出每一个单词在字符串s中可能的下标，判断以此为起点是否能够形成一个符合条件的子串。复杂度已经到阶乘了，超时案例我自己都想好了，比如什么'ababababa'，单词数组为['ab', 'ba']，这种字符串中有一半都满足，当量级上升时一定会超时。          
+Time Limit Exceeded
 
 - **优化思路**   
-  
+注意到条件中有一个比较特殊的条件是：words中的所有单词长度一致，对于words含有k个单词，单词长度为l。可以假设字符串有符合条件的子串，则其长度为k\*l，且以l个字符为一个单位，需要一一匹配words中单词，此处可以通过哈希表来确定唯一对应。   
+加入双指针a，b，a从字符串s的下标0开始，b位于字符串的下标k*l-1，指针a和b中间标识假设可行的子串，通过hash表匹配是否符合上述条件。   
+Runtime: 348 ms, faster than 50.61% of JavaScript online submissions for Substring with Concatenation of All Words.   
+Memory Usage: 43.7 MB, less than 50.00% of JavaScript online submissions for Substring with Concatenation of All Words.   
 
 - **高票答案对比**   
-最高票：
+最高票：https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/13656/An-O(N)-solution-with-detailed-explanation    
+这个思路与优化思路基本一致，有一个优化的点在于双指针遍历，优化思路指针遍历整个字符串s，实际上这中间有一些重复的判断，只需要遍历一个单词长度即可。这个优化基于words数组中每个单词长度一致，对于s = "barfoothefoobarman"，words = ["foo","bar"]，指针a从下标0开始遍历和从下标3（一个单词长度）开始遍历，有大量的重合。    
+Runtime: 80 ms, faster than 88.89% of JavaScript online submissions for Substring with Concatenation of All Words.   
+Memory Usage: 44.3 MB, less than 50.00% of JavaScript online submissions for Substring with Concatenation of All Words.    
--- src/0030SubstringWithConcatenationOfAllWords.js
+++ src/0030SubstringWithConcatenationOfAllWords.js
@@ -4,6 +4,118 @@
  * @param {string[]} words
  * @return {number[]}
  */
-var findSubstring = function(s, words) {
-    
+var findSubstringNormal = function(s, words) {
+  if (s.length === 0 || words.length === 0 || s.length < words.join('').length) return [];
+  let ret = [];
+  function checkValid(str, wordArr) {
+    for(let i=0; i<wordArr.length; i++) {
+      let word = wordArr[i];
+      if(str.indexOf(word) == 0) {
+        if (wordArr.length === 1) return true;
+        else {
+          let newWordsArr = wordArr;
+          newWordsArr.splice(i);
+          return checkValid(str.substr(word.length), newWordsArr);
+        }
+      } 
+    }
+    return false;
+  }
+  for(let i=0; i<words.length; i++) {
+    let word = words[i];
+    let temp = s;
+    let newWordsArr = words.filter(item, index => index!==i)
+    let preIdx = 0;
+    while(temp.indexOf(word) !== -1) {
+      let idx = temp.indexOf(word);
+      temp = temp.substr(temp.indexOf(word));
+      if(checkValid(temp, newWordsArr)) {
+        ret.push(idx+preIdx);
+      }
+      preIdx += idx;
+    }
+  }
+  return ret;
+};
+
+/**
+ * 优化思路
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstringBetter = function(s, words) {
+  if (s.length === 0 || words.length === 0 || s.length < words.join('').length) return [];
+  let ret = [];
+  let k = words.length;
+  let l = words[0].length;
+  for(let p1=0, p2=k*l; p1<s.length && p2<=s.length; p1++, p2++) {
+    let tempStr = s.substr(p1, p2);
+    // 初始化哈希table
+    let map = {};
+    words.forEach((item) => {
+      map[item] = map[item]?map[item]+1:1;
+    });
+    let success = true;
+    for(let j=0; j<k; j++) {
+      let currWord = tempStr.substr(j*l, l);
+      if(map[currWord] > 0) {
+        map[currWord]--;
+      } else {
+        success = false;
+        break;
+      }
+    }
+    if(success) ret.push(p1);
+  }
+  return ret;
+};
+
+/**
+ * 高票思路
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstringVoted = function(s, words) {
+  if (words.length === 0) return [];
+  let ret = [];
+  const wordNum = words.length;
+  const wordLen = words[0].length;
+  // 哈希table
+  let map = {};
+  words.forEach((item) => {
+    map[item] = map[item]?map[item]+1:1;
+  });
+  for(let p1=0; p1<wordLen; p1++) {
+    let tempMap = {};
+    let wordFound = 0; // 已匹配的word数量
+    let subStrSt = p1;
+    for(let j=p1; j<=s.length-wordLen; j+=wordLen) {
+      const currWord = s.substr(j, wordLen);
+      if(!map[currWord]) { // 不存在于words数组中
+        wordFound = 0;
+        tempMap = {};
+        subStrSt = j + wordLen;
+      } else if(tempMap[currWord] !== map[currWord]) { // 未匹配到的word
+        tempMap[currWord] = tempMap[currWord] ? tempMap[currWord]+1:1;
+        wordFound++;
+      } else { // 已匹配过的word
+        let wordToRemove;
+        // 找到之前匹配的word位置，从其后开始重新构造子串
+        while(wordToRemove !== currWord && subStrSt <= j) {
+          wordToRemove = s.substr(subStrSt, wordLen);
+          subStrSt += wordLen;
+          tempMap[wordToRemove] -= 1;
+          wordFound--;
+        }
+        wordFound++;
+        tempMap[currWord]++;
+      }
+      if (wordFound === wordNum) {
+        ret.push(j - wordLen*(wordNum-1));
+      }
+    }
+  }
+  return ret;
 };
