f668c60dc7a8abe5f79c4e8300460d3321c71cf3
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-11-03 01:34:52 +0800

    add: 0032

--- src/0032LongestValidParentheses.js
+++ src/0032LongestValidParentheses.js
@@ -1,38 +1,112 @@
 /**
+ * 第一思路
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function (s) {
-  var checkValid = function(s, l, r) {
-    let valid = 0;
-    for(let j=l; j<=r; j++) {
-      if(s[j] === '(') valid--;
-      else valid++;
-      if(valid > 0) return false;
-    }
-    console.log(valid)
-    return valid===0;
-  }
-  let depth = [];
-  let lastDepth = 0;
-  let left = {};
+var longestValidParenthesesNormal = function (s) {
   let ret = 0;
-  for(let i=0; i<s.length; i++) {
-    if (s[i] === ')') {
-      depth.push(++lastDepth);
-      if(lastDepth > 0) left = {};
-      if(left[lastDepth-1] !== undefined) {
-        if (ret >= i-left[lastDepth-1]+1 && checkValid(s, left[lastDepth-1], i)) {
-          ret = i-left[lastDepth-1]+1;
-        }
+  let prev = 0;
+
+  for (let i=0; i<s.length; i++) {
+    prev = 0;
+    for (let j=i; j<s.length; j++) {
+//       console.log(i, j, prev)
+      if(prev === false) break;
+
+      if(s[j] === '(') {
+        prev -= 1;
+      } else {
+        prev = prev<0 ? prev+1:false;
       }
+      if(prev === 0 && ret < j-i+1) ret = j-i+1;
+    }
+  }
+
+  return ret;
+};
+
+/**
+ * 优化思路
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParenthesesBetter = function(s) {
+  const Stack = () => {
+    let arr = [];
+    let len = 0;
+    const top = () => {
+      return len > 0 ? arr[len - 1] : null;
+    };
+    const push = ele => {
+      arr[len++] = ele;
+    };
+    const pop = () => {
+      if(len>0) {
+        len--;
+        return arr[len];
+      }
+      return null;
+    };
+    return {
+      top,
+      push,
+      pop
+    };
+  };
+
+  let stack = Stack();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push({
+        isLeft: true,
+        index: i
+      });
     } else {
-      depth.push(--lastDepth);
-      if(left[lastDepth] === undefined) {
-        left[lastDepth] = i;
+      let top = stack.top();
+      if (top && top.isLeft) stack.pop();
+      else {
+        stack.push({
+          isLeft: false,
+          index: i
+        });
       }
     }
   }
-  console.log(depth);
+
+  let lastIndex = s.length;
+  let ret = 0;
+  while (stack.top() !== null) {
+    let top = stack.pop();
+    if (lastIndex - top.index - 1 > ret) ret = lastIndex - top.index - 1;
+    lastIndex = top.index;
+  }
+
+  return ret > lastIndex ? ret : lastIndex;
+};
+
+
+/**
+ * 高票思路
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParenthesesVoted = function(s) {
+  let v = []; v[0] = 0;
+  let open = 0;
+  let ret = 0;
+  for (let i=1; i<=s.length; i++) {
+    if(s[i-1] === '(') {
+      v[i] = 0;
+      open++;
+    }
+    else if(open > 0) {
+      v[i] = v[i-1] + 2;
+      v[i] += v[i-v[i]];
+      open--;
+      if(v[i] > ret) ret = v[i];
+    } else {
+      v[i] = 0;
+    }
+  }
   return ret;
 };
