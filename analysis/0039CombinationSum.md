c8462aa8ed15f7790b803d5a97d1a700c93cd36e
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-11-17 23:02:34 +0800

    add: 0039

--- analysis/0039CombinationSum.md
+++ analysis/0039CombinationSum.md
@@ -25,7 +25,7 @@
   ]
   ```
 - **思路**   
-回溯，由于数字集合中的所有数字都可以无限次使用，所以每次的候选数字都是全集合；另外数字集合中不重复，所以可以不处理结果重复，思路如下。时间复杂度O(target^2)。    
+回溯，由于数字集合中的所有数字都可以无限次使用，所以每次的候选数字都是全集合；另外数字集合中不重复，所以可以不处理结果重复，思路如下。时间复杂度O(2^n)。    
   ```
   1. 对candidate进行从小到大排序
   2. 从下标index（初始为0）开始遍历数组
@@ -36,8 +36,29 @@
   Memory Usage: 37.3 MB, less than 26.67% of JavaScript online submissions for Combination Sum.   
 
 - **优化思路**   
-
+大体思路没有什么优化方向，可以考虑优化细节。比如递归的结束判断，如果放在循环中，每次循环中都需要判断，所以将其放在循环外，当到达条件时直接放入结果集中。   
+  ```
+  // 优化前
+  function recursion(target, preArr, index) {
+    for (let i=index; i<candidates.length; i++) {  
+      if (target === candidates[i]) ret.push([...preArr]);
+      ...
+    }
+  }
+  // 优化后
+  function recursion(target, preArr, index) {
+    if (target === 0) ret.push(preArr);
+    for (let i=index; i<candidates.length; i++) {
+      ...
+    }
+  }
+  ```
+  Runtime: 72 ms, faster than 81.95% of JavaScript online submissions for Combination Sum.
+  Memory Usage: 37.1 MB, less than 33.33% of JavaScript online submissions for Combination Sum.
 
 
 - **高票答案对比**   
-最高票：https://leetcode.com/problems/valid-sudoku/discuss/15472/Short%2BSimple-Java-using-Strings   
+最高票：https://leetcode.com/problems/combination-sum/discuss/16509/Iterative-Java-DP-solution   
+最高票同优化思路，讨论区给出了DP的方案，一般来说DP总是优于递归的，DP通过记录前阶段的结果，来避免重复的计算。   
+Runtime: 68 ms, faster than 92.21% of JavaScript online submissions for Combination Sum.   
+Memory Usage: 37.7 MB, less than 13.33% of JavaScript online submissions for Combination Sum.   
--- src/0039CombinationSum.js
+++ src/0039CombinationSum.js
@@ -4,22 +4,68 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSumNormal = function(candidates, target) {
-  function sortNumber(a,b) {
-    return a - b
-  }
-  candidates.sort(sortNumber);
-  const len = candidates.length;
+var combinationSumBetter = function(candidates, target) {
+  candidates.sort((a, b) => a - b);
   let ret = [];
-  function recursion(candidates_sorted, target, preArr, index) {
-    if (target < candidates_sorted[index]) return;
-    for (let i=index; i<len && target>=candidates_sorted[i]; i++) {
-      if (target === candidates_sorted[i]) ret.push(preArr.concat([candidates_sorted[i]]));
+  function recursion(target, preArr, index) {
+    if (target < candidates[index]) return;
+    for (let i=index; i<candidates.length && target>=candidates[i]; i++) {
+      preArr.push(candidates[i]);
+      if (target === candidates[i]) ret.push([...preArr]);
       else {
-        recursion(candidates_sorted, target-candidates_sorted[i], preArr.concat([candidates_sorted[i]]), i);
+        recursion(target-candidates[i], [...preArr], i);
       }
+      preArr.pop();
     }
   }
-  recursion(candidates, target, [], 0);
+  
+  recursion(target, [], 0);
   return ret;
+};
+
+/**
+ * 优化思路
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSumBetter = function(candidates, target) {
+  candidates.sort((a, b) => a - b);
+  let ret = [];
+  function recursion(target, preArr, index) {
+    if (target === 0) ret.push(preArr);
+    if (target < candidates[index]) return;
+    for (let i=index; i<candidates.length && target>=candidates[i]; i++) {
+      preArr.push(candidates[i]);
+      recursion(target-candidates[i], [...preArr], i);
+      preArr.pop();
+    }
+  }
+  
+  recursion(target, [], 0);
+  return ret;
+};
+
+
+var combinationSumVoted = function(candidates, target) {
+  candidates.sort((a, b) => a - b);
+  let dp = [];
+  // 1 ~ target的结果集合
+  for (let t=0; t<=target; t++) {
+    let combList = [];
+    for (let j=0; j<candidates.length && candidates[j]<=t; j++) {
+      if (t === candidates[j]) combList.push([candidates[j]]);
+      else if (i-candidates[j]-1 >= 0) {
+        for (const list in dp[i-candidates[j]]) {
+          if (candidates[j] <= list[0]) {
+            let cl = [candidates[j], ...list];
+            newList.push(cl);
+          }
+        }
+      }
+    }
+    dp[t] = newList;
+  }
+
+  return dp[target];
 };
