6a9f57632c18ff2c0f11848cc5b461db9fbbe443
caijieying <caijieying@jd.com>
2019-11-25 22:22:04 +0800

    add: 0044 50%

--- src/0044WildcardMatching.js
+++ src/0044WildcardMatching.js
@@ -5,5 +5,30 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-    
+  if (s.length==0) {
+    if (p.length==0 || p.replace('*', '')=='') return true;
+    else return false;
+  } else if (p.length==0) {
+    if (s.length==0) return true;
+    else return false;
+  }
+  let dp = [];
+  dp[0] = [true].concat(Array(s.length).fill(false))
+  // 初始化动态规划数组
+  for (let i=0; i<p.length; i++) {
+    dp[i+1] = [];
+    dp[i+1][0] = dp[i][0] && p[i]==='*';
+  }
+
+  for (let i=1; i<=s.length; i++) {
+    for (let j=1; j<=p.length; j++) {
+      if (p[j-1] === '?') {
+        dp[i][j] = dp[i-1][j-1]
+      } else if (p[j-1] === '*') {
+        dp[i][j] = dp[i-1][j-1]
+      }
+    }
+  }
+
+  return dp[p.length][s.length];
 };
