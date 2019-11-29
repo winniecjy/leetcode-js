163194a9d612c7cdbf76ebc365111327c90bed11
caijieying <caijieying@jd.com>
2019-11-29 18:05:28 +0800

    add: 0046

--- analysis/0046Permutations.md
+++ analysis/0046Permutations.md
@@ -15,16 +15,19 @@
   ]
   ```
 
-- **思路**   
+- **第一思路**   
 全排列问题，可以通过递归回溯实现。   
 Runtime: 68 ms, faster than 68.91% of JavaScript online submissions for Permutations.   
 Memory Usage: 37.6 MB, less than 8.00% of JavaScript online submissions for Permutations.   
 
 - **优化思路**   
-无。全排列的思路比较经典了，没有想到什么优化的地方。目前性能瓶颈应该是在内置函数的使用方案上了。            
+无。全排列的思路比较经典了，没有想到什么优化的地方，区别是在于递归的具体实现方案。            
 
 - **高票答案对比**   
-高票思路：https://leetcode.com/problems/permutations/discuss/431747/Javasctipt-56-ms-faster-than-99.05          
-
+高票思路：https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)              
+参考了一下高票思路和JavaScript区代码。总体其实都是递归。   
+高票作者的思路是从未包含的候选数组中，按顺序分别插入全排列结果中。对于当前排列是否包含候选者，通过indexOf来查找确认。    
+第一思路是循环固定全排列的第一个元素，剩下的数据再去进行全排列，拼接固定元素和全排列。   
+相比较于高票思路，第一思路的代码更简洁，但是递归重复计算部分会更多。    
 Runtime: 56 ms, faster than 98.97% of JavaScript online submissions for Permutations.   
 Memory Usage: 36.2 MB, less than 84.00% of JavaScript online submissions for Permutations.   
--- src/0046Permutations.js
+++ src/0046Permutations.js
@@ -3,14 +3,43 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
+var permute = function (nums) {
   if (nums.length == 0) return [[]]
   let res = [];
-  for(let i=0; i<nums.length; i++) {
-    const nextPermutation = permute(nums.slice(0,i).concat(nums.slice(i+1)));
-    for (let j=0; j<nextPermutation.length; j++) {
+  for (let i = 0; i < nums.length; i++) {
+    const nextPermutation = permute(nums.slice(0, i).concat(nums.slice(i + 1)));
+    for (let j = 0; j < nextPermutation.length; j++) {
       res.push([nums[i], ...nextPermutation[j]]);
     }
   }
+  return res;
+};
+
+
+/**
+ * 高票思路
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  if (!nums.length) return [];
+  let res = [];
+
+  function findPermutations(res, curr, nums) {
+    if (curr.length === nums.length) {
+      res.push([...curr]);
+      return;
+    }
+    for (let num of nums) {
+      if (curr.indexOf(num) === -1) {
+        curr.push(num);
+        findPermutations(res, curr, nums);
+        curr.pop(num);
+      }
+    }
+  }
+
+  findPermutations(res, [], nums);
+
   return res;
 };
