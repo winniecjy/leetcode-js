370fef9d6c3458b15f4eef632f6cc841224a5b5e
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-12-04 23:33:44 +0800

    add: 0050

--- analysis/0050Pow(x,n).md
+++ analysis/0050Pow(x,n).md
@@ -18,16 +18,24 @@
   - -100.0 < x < 100.0
   - n是一个32位有符号整数，大小范围为[-2^31, 2^31-1]
 - **第一思路**   
-按照给出的数据范围，可以肯定是大数运算。在大数运算中，幂运算是最复杂的一种，加减是底层运算，乘除基于加减，幂运算又基于乘除。如果直接进行运算，时间复杂度为O(n)。有题干可知n是32位有符号整数，当n极大时，程序运算时间会较长。           
+在所有运算之中，幂运算是最复杂的一种。如果直接进行运算，时间复杂度为O(n)。有题干可知n是32位有符号整数，当n极大时，程序运算时间会较长。           
 之前有见过幂乘题目的一个优化思路，以pow(5, 6)大概思路如下：   
   - 5^6 = 5^(4+2) = 5^4\*5^2   
     5^4 = 5^2\*5^2   
   - 将6转换为二进制表示6 = 4+2 = 2^2+2^1 = 110(2)    
     按照二进制顺序从右到左，2^(i+1) = 2^i+2^i    
-    5^4 = 5^(2^2) = 5^(2^1+2^1) = 5^(2^1) \* 5^(2^1)
-小数运算？？？
+    5^4 = 5^(2^2) = 5^(2^1+2^1) = 5^(2^1) \* 5^(2^1)   
+
+  对于n<0，可以将x转换为小数x^(-1*n) = (x^-1)^n，时间复杂度为O(logn)。
+  Runtime: 56 ms, faster than 80.47% of JavaScript online submissions for Pow(x, n).   
+  Memory Usage: 33.9 MB, less than 70.59% of JavaScript online submissions for Pow(x, n).   
+
 
 - **优化思路**   
-
+无
 
 - **高票答案对比**   
+高票思路：https://leetcode.com/problems/powx-n/discuss/19546/Short-and-easy-to-understand-solution   
+同第一思路，但是采用了递归实现，更加易于理解，代码更加简洁。    
+Runtime: 64 ms, faster than 36.29% of JavaScript online submissions for Pow(x, n).   
+Memory Usage: 33.9 MB, less than 47.06% of JavaScript online submissions for Pow(x, n).   
--- src/0050Pow(x,n).js
+++ src/0050Pow(x,n).js
@@ -4,30 +4,28 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-  if (n=0) return 1;
+var myPowNormal = function(x, n) {
+  if (n<0) x = 1/x;
   const binaryN = n.toString(2);
-  let base = '1';
+  let base = x;
+  let res = 1;
   for(let i=binaryN.length-1; i>=0; i--) {
-
+    const bit = binaryN[i];
+    if(bit === '1') res *= base;
+    base = base * base;
   }
+  return res;
 };
 
-function multiplyOper(num1, num2) {
-  const len1 = num1.length, len2 = num2.length;
-  let ret = Array(len1+len2).fill(0);
-  for (let i = len1 - 1; i >= 0; i--) {
-    for (let j = len2 - 1; j >= 0; j--) {
-      let curr = parseInt(num1[i]) * parseInt(num2[j]) + ret[i+j+1];
-      ret[i+j+1] = curr % 10;
-      ret[i+j] += parseInt(curr / 10);
-    }
-  }
-  ret = ret.join("");
-  let leadingzero = 0;
-  while (ret[leadingzero] === "0") {
-    leadingzero++;
-  }
-  ret = ret.slice(leadingzero > 0 ? leadingzero : 0);
-  return ret.length==0?'0':ret;
-}
+/**
+ * 高票思路
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPowVoted = function(x, n) {
+  if (n===0) return 1;
+  let powN = Math.abs(n);
+  let result = powN%2===0 ? myPow(x*x,powN/2) : myPow(x*x,(powN-1)/2) * x;
+  return n < 0 ? 1/result : result;
+};
