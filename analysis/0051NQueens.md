11b302cf514a24fd054d77421e6a23fc99dd4c8a
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-12-05 23:11:35 +0800

    add: 0051

--- analysis/0051NQueens.md
+++ analysis/0051NQueens.md
@@ -22,8 +22,25 @@ N皇后问题，在一个NxN的棋盘上放上N个皇后，使得皇后之间彼
   ]
   ```
 - **第一思路**   
-皇后之间不互相攻击的条件是不能位于同行同列和同一对角线
+皇后之间不互相攻击的条件是不能位于同行同列和同一对角线。可以通过回溯算法和哈希表来确定每一个皇后的位置。时间复杂度O(n^2)。        
+对于行列冲突的情况比较好确认，对于对角线需要通过两个式子来确认：   
+  ```
+  对于坐标(i, j)有两条对角线
+  1. 从右上角到左下角：当(i,j)与(m,n)位于同一对角线，则i+j=m+n
+  2. 从左上角到右下角：当(i,j)与(m,n)位于同一对角线，则i-j=m-n
+  ```  
+  Runtime: 88 ms, faster than 27.89% of JavaScript online submissions for N-Queens.   
+  Memory Usage: 38.4 MB, less than 100.00% of JavaScript online submissions for N-Queens.   
 - **优化思路**   
-
+无。   
 
 - **高票答案对比**   
+高票思路：https://leetcode.com/problems/n-queens/discuss/19808/Accepted-4ms-c%2B%2B-solution-use-backtracking-and-bitmask-easy-understand.   
+高票思路同第一思路，但是可以看到时间复杂度还是比较有优化空间的。所以参考了一下讨论区JavaScript答案，虽然都是回溯的算法，运行时间短的方案去除了哈希表。回溯的过程中用一个数组board记录每一行的列数，(i,board[i])表示皇后位置。检查(row, col)是否合法，直接遍历board数组，当`board[i] === col || i+board[i] === row+col || i-board === row-col`，则该位置不合法。   
+查了一些资料，有些博客提到了几个可能相关的JavaScript性能优化点：   
+  - 访问直接量和局部变量的速度最快，相反，访问数组元素和对象成员相对较慢。        
+  - 实现中使用了object来实现哈希表的效果，对object属性进行赋值时，需要查询当前对象及原型链上是否有相同属性。   
+
+  具体是否关联待实际测试（也有可能是LeetCode judge的问题_(:з」∠)_先mark了！    
+  Runtime: 60 ms, faster than 96.49% of JavaScript online submissions for N-Queens.   
+  Memory Usage: 36.9 MB, less than 100.00% of JavaScript online submissions for N-Queens.   
--- src/0051NQueens.js
+++ src/0051NQueens.js
@@ -3,6 +3,58 @@
  * @param {number} n
  * @return {string[][]}
  */
-var solveNQueens = function(n) {
-    
+var solveNQueensNormal = function(n) {
+  let ret = [];
+  function recursion(map, row, n, rows) {
+    if (row === n) ret.push(rows);
+    for(let col=0; col<n; col++) {
+      if (!map[`col${col}`] && !map[`dialogLR${row-col}`] && !map[`dialogRL${row+col}`]) {
+        map[`col${col}`] = true;
+        map[`dialogLR${row-col}`] = true;
+        map[`dialogRL${row+col}`] = true;
+        let str = Array(n).fill('.');
+        str[col] = 'Q';
+        rows.push(str.join(''));
+        recursion(map, row+1, n, [...rows]);
+        rows.pop();
+        map[`col${col}`] = false;
+        map[`dialogLR${row-col}`] = false;
+        map[`dialogRL${row+col}`] = false;
+      }
+    }
+  }
+  recursion({}, 0, n, []);
+  return ret;
+};
+
+
+/**
+ * 高票思路
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueensVoted = function(n) {
+  let ret = [];
+  function recursion(row, n, board, ret) {
+    if (row === n) {
+      const res = board.map(col => 
+        '.'.repeat(col) + 'Q' + '.'.repeat(n - col - 1))
+      ret.push(res);
+    }
+    for(let col=0; col<n; col++) {
+      if (checkValid(row, col, board)) {
+        board.push(col);
+        recursion(row+1, n, board, ret);
+        board.pop();
+      }
+    }
+  }
+  function checkValid(row, col, board) {
+    for(let i=0; i<board.length; i++) {
+      if(board[i] === col || i+board[i]===row+col || i-board[i]===row-col) return false;
+    }
+    return true;
+  }
+  recursion(0, n, [], ret);
+  return ret;
 };
